303c40f70141125cf2ad64812ebaff8ed675d317
PunithavathiK <asmipunitha@gmail.com>
2024-11-07 07:22:26 +0000

    Update create_sqs1.py

--- create_sqs1.py
+++ create_sqs1.py
@@ -1,12 +1,12 @@
 import boto3
 from botocore.exceptions import BotoCoreError, ClientError
 
-# AWS credentials (replace these with your actual values)
+# AWS credentials
 aws_access_key = '****************AB5A'  # Access Key
 aws_secret_key = '************************************JSWF'  # Secret Key
 region = 'us-west-1'  # Region
 
-# Create an SQS client with your custom credentials
+# Create an SQS client with the custom credentials
 sqs = boto3.client(
     'sqs',
     aws_access_key_id=aws_access_key,
@@ -14,7 +14,7 @@ sqs = boto3.client(
     region_name=region
 )
 
-# Queue parameters (customize this)
+# Queue parameters
 queue_name = 'unique-queue-12345'  # Unique queue name
 attributes = {
     'DelaySeconds': '0',  # (Optional) Set the delay (in seconds) for the messages
